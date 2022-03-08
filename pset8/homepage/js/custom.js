e43cd3d0128a40447bc6e399f6696fbb86f7966d
aViRaL NYCXC <78311382+aviralkaushik47@users.noreply.github.com>
2022-03-08 19:12:40 +0530

    Add files via upload

---
+++ pset8/homepage/js/custom.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let course_button = document.querySelector("#button_cs50");
+    course_button.addEventListener("click", function() {
+        window.open("https://cs50.harvard.edu/x/2021/", '_blank');
+    })
+
+    let my_solutions_button = document.querySelector("#button_my_solutions");
+    my_solutions_button.addEventListener("click", function() {
+        window.open("https://github.com/aviralkaushik47/CS50x2021.git", "_blank");
+    });
+
+
+    })
+
+ document.addEventListener("DOMContentLoaded", function() {
+    let lo_button = document.querySelector("#button_insta");
+    lo_button.addEventListener("click", function() {
+        window.open("https://www.instagram.com/_nycxc._/", '_blank');
+    })
+
+    let my_github = document.querySelector("#button_github");
+    my_github.addEventListener("click", function() {
+        window.open("https://github.com/aviralkaushik47/CS50x2021.git", "_blank");
+
+});
+
+})
